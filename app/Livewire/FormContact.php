985b0006495789bf22f505e79578c1371fa66200
soulksyoux <andre18july@gmail.com>
2024-12-29 10:23:32 +0000

    005 form contact created and valitation

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,10 +2,23 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+    public $name, $email, $phone;
+
+    public function newContact() {
+        $this->validate([
+            'name' => ['required', 'min:3', 'max:50'],
+            'email' => ['required', 'email', 'min:5', 'max:50'],
+            'phone' => ['required', 'min:5', 'max:20'],
+        ]);
+
+        Log::info("Novo contacto: " . $this->name . " - " . $this->email . " - " . $this->phone);
+    }
+
     public function render()
     {
         return view('livewire.form-contact');
--- resources/views/livewire/form-contact.blade.php
+++ resources/views/livewire/form-contact.blade.php
@@ -1,19 +1,30 @@
 <div class="card p-5">
-    <form>
+    <form wire:submit="newContact">
 
+        @csrf
+        
         <div class="mb-3">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name">
+            <input type="text" class="form-control" id="name" wire:model="name">
+            @error('name')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="email">Email</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" wire:model="email">
+            @error('email')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="phone">Phone</label>
-            <input type="phone" class="form-control" id="phone">
+            <input type="phone" class="form-control" id="phone" wire:model="phone">
+            @error('phone')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="text-end">
