b1df56dba2fa045d17bf7ebc0207a340387b8b11
soulksyoux <andre18july@gmail.com>
2024-12-29 10:51:02 +0000

    006 save data on db

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,21 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Models\Contact;
 use Illuminate\Support\Facades\Log;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FormContact extends Component
 {
-    public $name, $email, $phone;
+    #[Validate(['required', 'min:3', 'max:50'])]
+    public $name;
+
+    #[Validate(['required', 'email', 'min:5', 'max:50'])]
+    public $email;
+
+    #[Validate(['required', 'min:5', 'max:20'])]
+    public $phone;
 
     public function newContact() {
-        $this->validate([
-            'name' => ['required', 'min:3', 'max:50'],
-            'email' => ['required', 'email', 'min:5', 'max:50'],
-            'phone' => ['required', 'min:5', 'max:20'],
-        ]);
 
-        Log::info("Novo contacto: " . $this->name . " - " . $this->email . " - " . $this->phone);
+        $this->validate();
+
+        // $this->validate([
+        //     'name' => ['required', 'min:3', 'max:50'],
+        //     'email' => ['required', 'email', 'min:5', 'max:50'],
+        //     'phone' => ['required', 'min:5', 'max:20'],
+        // ]);
+
+        Contact::firstOrCreate(
+            [
+                'name' => $this->name,
+                'email' => $this->email
+            ],
+            [
+                'phone' => $this->phone
+            ]
+        );
+
+        $this->reset();
     }
 
     public function render()
